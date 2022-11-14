45d47a392e8754834f6c6bb89551b0a536c9b257
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2022-11-14 19:41:54 +0530

    Update blogs.js

--- pages/blogs.js
+++ pages/blogs.js
@@ -28,7 +28,7 @@ const Blogs = ({ blog, profile }) => {
 
       <Head>
         <meta charset="utf-8" />
-        <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
+        <meta content="IE=edge,chrome=1" httpEquiv="X-UA-Compatible" />
         <meta content="width=device-width, initial-scale=1, shrink-to-fit=no" name="viewport" />
         <title>{blog.title}</title>
         <meta property="og:title" content="Data Science Portfolio" />
@@ -44,7 +44,7 @@ const Blogs = ({ blog, profile }) => {
         <meta property="og:image" content="//assets/img/social.jpg" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@tailwindmade" />
-        <link crossorigin="crossorigin" href="https://fonts.gstatic.com" rel="preconnect" />
+        <link crossOrigin="crossorigin" href="https://fonts.gstatic.com" rel="preconnect" />
 
         <link as="style"
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500;600;700&display=swap"
@@ -53,7 +53,7 @@ const Blogs = ({ blog, profile }) => {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@400;500;600;700&display=swap"
           rel="stylesheet" />
         <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet" />
-        <link crossorigin="anonymous" href="/assets/styles/main.min.css" media="screen" rel="stylesheet" />
+        <link crossOrigin="anonymous" href="/assets/styles/main.min.css" media="screen" rel="stylesheet" />
         <script defer src="https://unpkg.com/@alpine-collective/toolkit@1.0.0/dist/cdn.min.js"></script>
         <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <link rel="stylesheet"
