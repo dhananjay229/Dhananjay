91611e513d0bd6c4d185875b277d9a2912a7955a
dhananjay229 <dhananjay45423@gmail.com>
2023-06-28 17:44:33 +0530

    Fix image adjustment

--- pages/index.js
+++ pages/index.js
@@ -352,11 +352,11 @@ export default function Home({ blog,project }) {
 
           {project.map((item) => {
             return <Link key={item.slug.current} href={"/project/" + item.slug.current} className="shadow">
-              <div className="overflow-hidden  aspect-video bg-red-400 cursor-pointer rounded-xl relative group">
+              <div className="overflow-hidden cursor-pointer rounded-xl relative group">
                 <div className="rounded-xl z-50 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out cursor-pointer
                  absolute from-black/80 to-transparent bg-gradient-to-t inset-x-0 -bottom-2 pt-30 text-white flex items-end">
                   <div>
-                    <div className="transform-gpu  p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 
+                    <div className=" p-4 space-y-3 text-xl group-hover:opacity-100 group-hover:translate-y-0 
                     translate-y-4 pb-10 transform transition duration-300 ease-in-out">
                       <div className="font-bold text-cyan-500">{item.title}</div>
                       <div className="opacity-60 text-sm text-cyan-300 ">{item.metadesc}</div>
