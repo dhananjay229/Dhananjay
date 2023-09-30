164cda69ac6feb7f6baa8da9613c344596b48b48
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2023-09-30 21:43:58 +0530

    Update about.js
    Update resume name

--- pages/about.js
+++ pages/about.js
@@ -15,14 +15,14 @@ import NavBar from '../components/NavBar';
 
 const onButtonClick = () => {
   // using Java Script method to get PDF file
-  fetch('resume.pdf').then(response => {
+  fetch('Dhananjay_resume.pdf').then(response => {
     response.blob().then(blob => {
       // Creating new object of PDF file
       const fileURL = window.URL.createObjectURL(blob);
       // Setting various property values
       let alink = document.createElement('a');
       alink.href = fileURL;
-      alink.download = 'resume.pdf';
+      alink.download = 'Dhananjay_resume.pdf';
       alink.click();
     })
   })
